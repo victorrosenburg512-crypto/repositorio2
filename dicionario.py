e59633751fd90ce524e155cf6d7ea93366edc6fe
victorrosenburg512-crypto <victorrosenburg512@gmail.com>
2026-04-27 19:29:07 -0300

    Update dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -2,7 +2,7 @@ for i in range(5):
     meme_dict={
         "STALKEAR" :  "investigar a vida de alguém online",
         "CRINGE"   :  "algo vergonhoso ou constrangedor",
-        "VDD"  :   "abreviação da palavra 'verdade' ",
+        "VDD"  :   "abreviação da palavra 'verdade.' ",
         "BISCOITAR" : "postar algo apenas para chamar a atenção",
         "HATER" : 'pessoa que está constantemente criticando os outros',
         "VLW" :  "abreviação da palavra 'valeu'"
